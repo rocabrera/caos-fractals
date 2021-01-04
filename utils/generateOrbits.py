6c5db66f38c2f1095b09661904300092c3766152
Rodrigo Cabrera Castaldoni <rodrigo.ccastaldoni@hsl.org.br>
2021-01-03 23:40:17 -0300

    biffurcation graph

--- utils/generateOrbits.py
+++ utils/generateOrbits.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+import sys   
+sys.setrecursionlimit(10**6) 
+
 def last_value_orbit(func, x0, n):
 
     if n == 0: return x0
@@ -16,3 +19,13 @@ def generate_orbit(func, x0, n, lst):
     lst.append(x0)
     next_x0 = func(x0)
     return generate_orbit(func, next_x0, n-1, lst)
+
+def generate_orbit_iter(func, x0, steps):
+    val = x0
+    orbit_results = [val]
+    for _ in range(steps):
+        next_val = func(val)
+        orbit_results.append(next_val)
+        val = next_val
+        
+    return orbit_results
--- utils/plots.py
+++ utils/plots.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from functools import partial
+from utils.generateOrbits import generate_orbit_iter
 
 def plot_style(ax, title, xlabel, ylabel):
     if ax:
@@ -47,3 +49,36 @@ def plot_func(func, lims, title, xlabel, ylabel, ax=None):
     ax.legend()
     
     return line1, line2
+
+#########
+
+def plot_bifurcation_diagram(func,x0, steps, r_step, round_param, rrange, labels, ax=None):
+    
+    
+    title, xlabel, ylabel = labels
+    ax = ax or plt.gca()
+    plot_style(ax, title, xlabel, ylabel)
+        
+    initial_r, final_r = rrange
+    rs = np.arange(initial_r, final_r+r_step, r_step)
+    transition_r = []
+    aux = 1
+    
+    lines = []
+    for r in rs:
+        func_r = partial(func, r)
+
+        y = generate_orbit_iter(func_r, x0, steps)
+        final_states = set(map(lambda x: round(x,round_param), y[-100:]))
+        
+        n_periods = len(final_states)
+        for final_state in final_states:            
+            if aux < n_periods :
+                aux = n_periods
+                transition_r.append(r)
+            
+            lines.append(ax.scatter(r, final_state, c = "r", s = 5))
+            
+    ax.legend()
+    
+    return transition_r,lines 
